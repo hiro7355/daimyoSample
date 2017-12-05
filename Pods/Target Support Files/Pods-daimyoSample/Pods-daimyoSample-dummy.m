67d9183783dd018ff08d59bd810d2e22cc354664
michihiro nozawa <michihiro_nozawa@ikaika.co.jp>
2017-12-05 17:39:50 +0900

    Podsフォルダ追加

---
+++ Pods/Target Support Files/Pods-daimyoSample/Pods-daimyoSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_daimyoSample : NSObject
+@end
+@implementation PodsDummy_Pods_daimyoSample
+@end
---
+++ Pods/Target Support Files/Pods-daimyoSampleTests/Pods-daimyoSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_daimyoSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_daimyoSampleTests
+@end
